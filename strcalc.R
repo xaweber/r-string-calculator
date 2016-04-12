ef9942567d464f3ee28b9f9a471a89bd26e43f64
xaweber <xenia.a.weber@gmail.com>
2016-04-12 15:26:31 +1000

    strcalc.R

---
+++ strcalc.R
@@ -0,0 +1,12 @@
+compute = function(input_string) {
+  values= unlist(strsplit(input_string, ''))
+  num0 = as.integer(values[1])
+  operator = values[2]
+  num1 = as.integer(values[3])
+  if (operator == 'x') {
+    return(num0+num1)
+  } else {
+    print('unknown operator!')
+    return(0)
+  }
+  }
