ab5836b75a4505ac4f676128598edd06af64ca73
xaweber <xenia.a.weber@gmail.com>
2016-04-12 17:03:12 +1000

    modified =

--- strcalc.R
+++ strcalc.R
@@ -1,10 +1,10 @@
-compute = function(input_string) {
+compute<-function(input_string) {
   #Do simple arithmetic from strings
   #Example: compute ('5+2') -> 7
-  values= unlist(strsplit(input_string, ''))
-  num0 = as.integer(values[1])
-  operator = values[2]
-  num1 = as.integer(values[3])
+  values <- unlist(strsplit(input_string, ''))
+  num0<-as.integer(values[1])
+  operator<-values[2]
+  num1<-as.integer(values[3])
   result<-switch(operator,
                  '+' = num0 + num1,
                  '-' = num0 - num1,
